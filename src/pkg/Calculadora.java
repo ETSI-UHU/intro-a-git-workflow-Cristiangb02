74598b1463586342e94865c23e5c5f32e5057658
raulp <raulp@DESKTOP-24HE6PO>
2025-03-07 12:37:46 +0100

    Refactorización del método resta

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -26,7 +26,7 @@ public class Calculadora {
                 System.out.println("Resultado: " + resultado);
                 break;
             case '-':
-                resultado = num1 - num2;
+            	resultado = resta(num1, num2);
                 System.out.println("Resultado: " + resultado);
                 break;
             case '*':
@@ -47,4 +47,10 @@ public class Calculadora {
 
         scanner.close();
     }
+
+	private static double resta(double num1, double num2) {
+		double resultado;
+		resultado = num1 - num2;
+		return resultado;
+	}
 }
